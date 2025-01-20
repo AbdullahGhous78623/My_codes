ba78ea11581197c646d0f5f5b52da2f785839fc3
Abdullah Ghous <abdullahghous78623@gmail.com>
2025-01-20 19:11:41 +0530

    New codes written

--- january_crash_code/revision.py
+++ january_crash_code/revision.py
@@ -1,64 +1,19 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+a=[4,5,7,56,456,4,365,246]
 
-class LinkedList:
-    def __init__(self):
-        self.head=None
-        self.n=0
-    def __len__(self):
-        return self.n
-    def insert_head(self,data):
-        new_node=Node(data)
-        new_node.next=self.head
-        self.head=new_node
-        self.n+=1
-    def __str__(self):
-        str=""
-        reference=self.head
-        while reference!=None:
-            str+=f"{reference.data}"+"->"
-            reference=reference.next
-        return str[:-2]
-    def append(self,data):
-        new_node=Node(data)
-        self.n+=1
-        reference=self.head
-        if self.head==None:
-            self.head=new_node
-            
-            return
-        
-        while reference.next:
-            reference=reference.next
-        reference.next=new_node
-    def insert_after(self,after,data):
-        new_node=Node(data)
-        reference=self.head
-        while reference!=None:
-            
-            if reference.data==after:
-                break
-            reference=reference.next
-        if reference==None:
-            print(f"Value {after} not found in the linked list.") 
-        else:
-            new_node.next=reference.next
-            reference.next=new_node
-            self.n+=1
-    def clear(self):
-        self.head=None
-        self.n=0
+greatest=a[0]
 
-
-L=LinkedList()
-L.insert_middle(8,12)
-
-
+for i in a[1:]:
+    if i>greatest:
+        greatest=i
     
+while greatest in a:
+    a.remove(greatest)
+print(a)
+sec_greatest=a[0]
+for j in a[1:]:
+    if i>sec_greatest:
+        sec_greatest=j
 
-
-
+print(sec_greatest)
 
 
